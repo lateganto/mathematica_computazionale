2f0cf706daa0007fe60a9d609773750850cf0cdf
Davide Montanari <davide.montanari10@gmail.com>
2018-04-27 12:30:02 +0200

    Update

--- package.m
+++ package.m
@@ -27,53 +27,77 @@ drawCircle::usage = "disegna un grafico";
 Begin["`Private`"]; (* Comincia spazio privato *)
 getAngle[p1_,p2_] := Mod[ArcTan @@ (p2 - p1), 2 Pi];  
 
-drawCircle[] := Manipulate[
-  Row[{
-    Graphics[{
-      Circle[],
-      Point[{0,0}],
-      PointSize[Large],
-      Circle[{0,0},0.2,{0,x}],
-      {Dashed, Line[{{0,0},{Cos[x],Sin[x]}}]},(*raggio*)
-      Thickness[0.01],
-      RGBColor[0,255,0],
-      Line[{{0,0},{Cos[x],0}}],
-      RGBColor[255,0,0],
-      Line[{{Cos[x],0},{Cos[x],Sin[x]}}], 
-      {Yellow, PointSize @ .02, Point@{Cos[x], Sin[x]}}},
-      ImageSize->350,
-      Axes->True
-    ] (*FINE GRAPHICS*),
-    Column[{
-        Show[
-          Plot[Sin[y], {y, 0,  2 Pi}, 
-            ImageSize->300, 
-            Ticks -> {{0, Pi/2, Pi, 3 Pi/2, 2 Pi, 5 Pi/2}, {-1, 0, 1}},
-            PlotLabel -> "Sine function",
-            PlotStyle -> {Orange}
-          ], 
-          Graphics[{ 
-            {Dashed, Line[{{x,0},{x,Sin[x]}}]},
-            PointSize[Large],
-            Point[{ x, Sin[x] }]}
-          ]
-        ],
-        Show[
-          Plot[Cos[y], {y, 0,  2 Pi}, 
-            ImageSize->300,
-            Ticks -> {{0, Pi/2, Pi, 3 Pi/2, 2 Pi, 5 Pi/2}, {-1, 0, 1}},
-            PlotLabel -> "Cosine function",
-            PlotStyle -> {Blue}
+FromRadToGrad[x_] := Return[x*180/Pi];
+FromGradToRad[x_] := Return[N[Pi*x/180]];
+
+drawCircle[] := 
+
+DynamicModule[{quad, start, end,x},
+  
+  Manipulate[
+    
+    x = FromGradToRad[grad];
+
+    Row[{
+      
+      Button["0\[Degree]",grad=0],Button["30\[Degree]",grad=30],
+			Button["45\[Degree]",grad=45],Button["60\[Degree]",grad=60],
+			Button["90\[Degree]",grad=90],"\n",
+			If[Cos[x]>=0&&Sin[x]>=0, quad=1;start=0; end=x;,
+				If[Cos[x]<0&&Sin[x]>0, quad=2;start=Pi; end= x;,
+					If[Cos[x]<0&&Sin[x]<0, quad=3;start=Pi; end= x;, quad= 4; start=0; end= -(2 Pi - x);]				
+			    ]
+			],
+			"Quadrante: ", quad, "\n",
+			"Angolo: ", grad, "\[Degree] || ", x,"\n",
+			"Coseno -> ", Cos[Pi/2], " ||| Seno -> ", Sin[x], "\n",
+
+      Graphics[{
+        Circle[],
+        Point[{0,0}],
+        PointSize[Large],
+        Circle[{0,0},0.2,{0,x}],
+        {Dashed, Line[{{0,0},{Cos[x],Sin[x]}}]},(*raggio*)
+        Thickness[0.01],
+        RGBColor[0,255,0],
+        Line[{{0,0},{Cos[x],0}}],
+        RGBColor[255,0,0],
+        Line[{{Cos[x],0},{Cos[x],Sin[x]}}], 
+        {Yellow, PointSize @ .02, Point@{Cos[x], Sin[x]}}},
+        ImageSize->350,
+        Axes->True
+      ] (*FINE GRAPHICS*),
+      
+      Column[{
+          Show[
+            Plot[Sin[y], {y, 0,  2 Pi}, 
+              ImageSize->300, 
+              Ticks -> {{0, Pi/2, Pi, 3 Pi/2, 2 Pi, 5 Pi/2}, {-1, 0, 1}},
+              PlotLabel -> "Sine function",
+              PlotStyle -> {Orange}
+            ], 
+            Graphics[{ 
+              {Dashed, Line[{{x,0},{x,Sin[x]}}]},
+              PointSize[Large],
+              Point[{ x, Sin[x] }]}
+            ]
           ],
-          Graphics[{
-            {Dashed, Line[{{x,0},{x,Cos[x]}}]},
-            PointSize[Large],
-            Point[{ x, Cos[x]}]}
+          Show[
+            Plot[Cos[y], {y, 0,  2 Pi}, 
+              ImageSize->300,
+              Ticks -> {{0, Pi/2, Pi, 3 Pi/2, 2 Pi, 5 Pi/2}, {-1, 0, 1}},
+              PlotLabel -> "Cosine function",
+              PlotStyle -> {Blue}
+            ],
+            Graphics[{
+              {Dashed, Line[{{x,0},{x,Cos[x]}}]},
+              PointSize[Large],
+              Point[{ x, Cos[x]}]}
+            ]
           ]
-        ]
-    }]
-  }],
-  {{x,0,"Angolo"}, 0, 2 Pi}
+      }]
+    }],
+    {{x,0,"Angolo"}, 0, 2 Pi}
 ];
 
 End[]; (* fine sezione privata *)
