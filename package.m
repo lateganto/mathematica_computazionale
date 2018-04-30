fc1bc450fb6bb1ee741a3cb72b861c8f9583ce97
antonioconte <antonio.conte05@live.it>
2018-04-30 12:06:07 +0200

    gioco del quadrante

--- package.m
+++ package.m
@@ -25,11 +25,46 @@ ClearAll["ProvaProgetto` *"];
 
 
 drawCircle::usage = "disegna un grafico";
+drawCartesian::usage = "disegna assi"
+startGame::usage = "gioco del quadrante";
+
 Begin["`Private`"]; (* Comincia spazio privato *)
 
 FromRadToGrad[x_] := Return[x*180/Pi];
 FromGradToRad[x_] := Return[N[Pi*x/180]];
 getAngle[p1_,p2_] := Mod[ArcTan @@ (p2 - p1), 2 Pi];  
+pointList = List[{3,2},{1,3},{-1,1},
+				{-2,-5},{-3,8},{-1,3},
+				{-2,1},{-7,3}];
+
+
+drawCartesian[x_,y_]:= DynamicModule[{limit = 10},
+
+	q1 = {limit,limit};
+	q2 = {-limit,limit};
+	q3 = {-limit,-limit};
+	q4 = {limit,-limit};
+	Graphics[{
+		Style[Rectangle[{0,0},q1],White], 
+		Style[Rectangle[{0,0},q2],White],
+		Style[Rectangle[{0,0},q3],White],
+		Style[Rectangle[{0,0},q4],White],
+		{PointSize[Large],Red,Point[{x,y}]}
+	}(* Fine Graphics *),	
+	Axes->True,
+	AxesLabel->{"x","y"},
+	Ticks->{
+		Table[i,{i,-limit, limit, 1}],
+		Table[i,{i,-limit, limit, 1}]
+		}
+	]
+];
+
+startGame[] := DynamicModule[{},
+	 random = RandomInteger[{1,Length[pointList]}];
+	 P = pointList[[random]];
+	 drawCartesian[P[[1]],P[[2]]]
+];
 
 GetQuad[x0_,y0_] := DynamicModule[{x=x0, y=y0, quad},
 	If[x>=0&&y>=0, quad=1];
@@ -51,8 +86,7 @@ drawCircle[] := Manipulate[
       Line[{{0,0},{Cos[x],0}}],
       RGBColor[255,0,0],
       Line[{{Cos[x],0},{Cos[x],Sin[x]}}],
-      A = IntegerString[FromRadToGrad[x]];
-      Text[Style[A <> "\[Degree]",Large,Red],{-0.90,1}],
+      Text[Style[IntegerString[FromRadToGrad[x]] <> "\[Degree]",Large,Red],{-0.90,1}],
       Text[Style["Seno",Medium,Red],{0.8,1}],
       Text[Style["Coseno",Medium,Green],{0.8,0.9}],
 
@@ -101,6 +135,14 @@ drawCircle[] := Manipulate[
 
 
 
+
+
+(* ::InheritFromParent:: *)
+(**)
+
+
+(* ::InheritFromParent:: *)
+(**)
 
 
 (* ::InheritFromParent:: *)
