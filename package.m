42ce9bf6893f129956e675dc643ea86c50b611de
davidmantegna <davide.montanari10@gmail.com>
2018-05-11 17:54:01 +0200

    Update package.m

--- package.m
+++ package.m
@@ -1,20 +1,3 @@
-(* ::Package:: *)
-
-(* ::Input:: *)
-(*(* PACKAGE.M*)
-(* * Progetto d'esame di Matematica Computazionale + Calcolo Numerico e Software Didattico*)
-(* * Corsi di laurea magistrale in Informatica e Matematica*)
-(* * Anno accademico 2017/2018*)
-(* *)
-(* * Autori:*)
-(* *   *)
-(* **)
-(* * Versione di sviluppo: Wolfram Mathematica 11.1*)
-(* * Versione di testing: Wolfram Mathematica 11.1 (Windows), 11.0 (Windows) e 10.2 (Debian)*)
-(* *)*)
-(**)
-
-
 BeginPackage[ "ProvaProgetto`"];
 Unprotect["ProvaProgetto` *"] (* toglie temporaneamente la protezione per ridefinire le funzioni *)
 ClearAll["ProvaProgetto` *"];
@@ -23,130 +6,8 @@ ClearAll["ProvaProgetto` *"];
 (* ::InheritFromParent:: *)
 (**)
 
-
-drawCircle::usage = "disegna un grafico";
-drawCartesian::usage = "disegna assi"
-startGame::usage = "gioco del quadrante";
-distanceGame::usage = "calcola la distanza distanza tra due punti";
 es4::usage = "secante/tangente";
 Begin["`Private`"]; (* Comincia spazio privato *)
-
-FromRadToGrad[x_] := Return[x*180/Pi];
-FromGradToRad[x_] := Return[N[Pi*x/180]];
-getAngle[p1_,p2_] := Mod[ArcTan @@ (p2 - p1), 2 Pi];  
-pointList = List[{3,2},{1,3},{-1,1},
-				{-2,-5},{-3,8},{-1,3},
-				{-2,1},{-7,3},{-10, 1}, {5,2}];
-
-
-(* ::InheritFromParent:: *)
-(**)
-
-
-drawCartesian[x_,y_]:= DynamicModule[{colEsito=Black, esito="",limit = 10, xIn=0, yIn=0},
-Row[{ 
-	q1 = {limit,limit};
-	q2 = {-limit,limit};
-	q3 = {-limit,-limit};
-	q4 = {limit,-limit};
-	Column[{
-	Graphics[{
-		Style[Rectangle[{0,0},q1],White], 
-		Style[Rectangle[{0,0},q2],White],
-		Style[Rectangle[{0,0},q3],White],
-		Style[Rectangle[{0,0},q4],White],
-		{PointSize[Large],Red,Point[{x,y}]}
-	},	
-	Axes->True,
-	ImageSize->450,
-	AxesLabel->{"x","y"},
-	Ticks->{
-		Table[i,{i,-limit, limit, 1}],
-		Table[i,{i,-limit, limit, 1}]
-		}
-	](* Fine Graphics *)
-	}],(*Fine Column*)
-	Column[{
-	Dynamic[Text[Style[esito,colEsito, 20]]],
-	"   x: "InputField[Dynamic[xIn],Number,FieldSize->5], "\n",
-	"   y: "InputField[Dynamic[yIn],Number,FieldSize->5],
-	Button["Risolvi", 
-		If[xIn == x && yIn ==y,
-			(*IF*)
-		  {esito:="Corretto"; colEsito:=Green}, 
-		   (*ELSE *)
-		  {esito:="Sbagliato"; colEsito:=Red }] 
-	]}]
-	}]Fine Row
-];
-
-startGame[] := DynamicModule[{},
-	 random = RandomInteger[{1,Length[pointList]}];
-	 P = pointList[[random]];
-	 drawCartesian[P[[1]],P[[2]]];
-   es4[]
-];
-
-GetQuad[x0_,y0_] := DynamicModule[{x=x0, y=y0, quad},
-	If[x>=0&&y>=0, quad=1];
-	If[x<0&&y>=0, quad=2];
-	If[x<=0&&y<=0, quad=3];
-	If[x>=0&&y<0, quad=4];
-quad];
-
-drawCircle[] := Manipulate[
-  Row[{
-    Graphics[{
-      Circle[],
-      Point[{0,0}],
-      PointSize[Large],
-      Circle[{0,0},0.2,{0,x}],
-      {Dashed, Line[{{0,0},{Cos[x],Sin[x]}}]},(*raggio*)
-      Thickness[0.01],
-      RGBColor[0,255,0],
-      Line[{{0,0},{Cos[x],0}}],
-      RGBColor[255,0,0],
-      Line[{{Cos[x],0},{Cos[x],Sin[x]}}],
-      Text[Style[IntegerString[FromRadToGrad[x]] <> "\[Degree]",Large,Red],{-0.90,1}],
-      Text[Style["Seno",Medium,Red],{0.8,1}],
-      Text[Style["Coseno",Medium,Green],{0.8,0.9}],
-
-      {Black,PointSize ->.02, Point[{Cos[x], Sin[x]}]}},
-      ImageSize->350,
-      Axes->True
-    ] (*FINE GRAPHICS*),
-    Column[{
-        (* GRAFICI SENO E COSENO *)
-        Show[
-          Plot[Sin[y], {y, 0,  2 Pi}, 
-            ImageSize->300, 
-            Ticks -> {{0, Pi/2, Pi, 3 Pi/2, 2 Pi, 5 Pi/2}, {-1, 0, 1}},
-            PlotLabel -> "Sine function",
-            PlotStyle -> {Orange}
-          ], 
-          Graphics[{ 
-            {Dashed, Line[{{x,0},{x,Sin[x]}}]},
-            PointSize[Large],
-            Point[{ x, Sin[x] }]}
-          ]
-        ],
-        Show[
-          Plot[Cos[y], {y, 0,  2 Pi}, 
-            ImageSize->300,
-            Ticks -> {{0, Pi/2, Pi, 3 Pi/2, 2 Pi, 5 Pi/2}, {-1, 0, 1}},
-            PlotLabel -> "Cosine function",
-            PlotStyle -> {Blue}
-          ],
-          Graphics[{
-            {Dashed, Line[{{x,0},{x,Cos[x]}}]},
-            PointSize[Large],
-            Point[{ x, Cos[x]}]}
-          ]
-        ]
-    }]
-  }],
-  {{x,0,"Angolo"}, 0, 2 Pi, 2 Pi/360}
-];
 
 secantList  = List[ {{0,-20}, {18,18}}, {{-20,9}, {18,6}}, {{-10,-13}, {6,20}} ];
 
@@ -249,17 +110,5 @@ es4[] := DynamicModule[{},
         }]
 ];
 
-(* ::InheritFromParent:: *)
-(**)
-
-
-(* ::InheritFromParent:: *)
-(**)
-
-
-(* ::InheritFromParent:: *)
-(**)
-
-
-End[]; (* fine sezione privata *)
-EndPackage[]; (* Fine del Package *)
+End[];
+EndPackage[];
